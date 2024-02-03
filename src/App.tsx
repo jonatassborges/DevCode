151bfb26a2f0c1a2b3b78f0b9317caec2a5474fe
jonatassborges <jonatasbnino@gmail.com>
2024-02-03 15:41:01 -0300

    HOME html

---
+++ src/App.tsx
@@ -0,0 +1,268 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <div>
+          <h1>Logo</h1> 
+        </div>
+        <ul>
+          <li>
+            <a href='#'>Home</a>
+          </li>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Services</a>
+          </li>
+          <li>
+            <a href='#'>Depoiments</a>
+          </li>
+        </ul>
+        <div>
+          <button>Contact</button>
+        </div>
+      </nav>
+    <header>
+      <section>
+        <div>
+          Desenvolvendo Sistemas e Aplicações WEB <br />
+          <span className='degrade'>de Alta Performance e Desempenho</span>
+        </div>
+        <div>
+          <h4>
+            Conectando eficiência e velocidade: Desenvolvendo o futuro da web. <br />
+            hoje.
+            <br />
+            <br />
+          </h4>
+          <h4>EXPLORE NOSSAS INFORMAÇÕES</h4>
+        </div>
+      </section>
+    </header>
+    <div className="degrade">
+      
+    </div>
+    <section>
+      <div>
+        <img src="" alt="" />
+        <h3>Estamos prontos para trilhar o caminho do desenvolvimento web com expertise e paixão, criando soluções de alta performance que refletem anos de conhecimento consolidado.</h3>
+      </div>
+    </section>
+    <section>
+      <h1>Conheça nosso time!</h1>
+      <section>
+        <div className='team-card'></div>
+        <div className='team-card'></div>
+        <div>
+          <div>
+            <div>
+              <img src="" alt="" />
+              <h3>Nota 4.9 de 5</h3>
+              <p>Quem faz aprova! Essa é a média de <br />avaliação dos nossos clientes .</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h3>+10.000 horas de desenvolvimento</h3>
+              <p>Em cada linha de código, refletimos as 10.000 horas dedicadas ao desenvolvimento, transformando experiência em inovação, e paixão em soluções de alta performance.</p>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <h3>+20 projetos Desenvolvidos</h3>
+              <p>Você vai aprender na prática com <br /> diversos projetos e cases reais pra evoluir <br /> o seu portfólio, além de colecionar <br />certificados para o seu currículo.</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src="" alt="" />
+              <h3>Entre em Contato Conosco!</h3>
+              <button>CONTATO (SVG)</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+    <div className='background'>
+      <div className="geral">
+        <h1>
+          Indepedente da sua necessidade a B&B DevCode está com você!
+        </h1>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div><img src="" alt="" /></div>
+        <div className="geralbaixo">
+          <div>
+            <h3>Também é para quem_</h3>
+          </div>
+          <div>
+            <div><img src="" alt="" />Quer um E-commerce de alta performance ou  um site para empresa</div>
+            <div><img src="" alt="" />Quer contratar desenvolvimento por Projeto</div>
+            <div><img src="" alt="" />Quer gerenciar sua empresa com alta qualidade (ORM)</div>
+            <div><img src="" alt="" />Quer um sistema complexo e otimizado</div>
+          </div>
+        </div>
+      </div>
+      <div className="degrade">
+        <h2>Transforme sua empresa com um sistema B&B DevCode</h2>
+        <img src="" alt="" />
+        <button>Contato</button>
+      </div>
+    </div>
+    <div className="backtech">
+      <div>
+      <h1>Chega de comprar sites com péssimo desempenho! Com agente você encontra Desempenho e Qualidade no mesmo lugar!</h1>
+      <h3>Veja todos os benefícios de quem usa nossas aplicações.</h3>
+      <img src="" alt="" />
+      </div>
+      <div className="features">
+        <div className="feature1">
+          <div>
+            <h2>
+            Tecnologias de acordo com a necessidade dos nossos clientes
+            </h2>
+            <p>
+            Adaptamos nossas tecnologias para melhor usabilidade do cliente.
+              Desenvolvendo de acordo com  as suas necessidades.
+            </p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="feature2">
+          <div>
+            <h2>
+            Sites Dinâmicos, de Alta Performance e Desempenho!
+            </h2>
+            <p>
+            Nossos sistemas  possuem alta escalabilidade e  Performance, sendo rigorosamente testados.
+            </p>
+            <img src="" alt="" /> <p></p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="feature3">
+          <div>
+            <h2>
+            Design de alta usabilidade
+            </h2>
+            <p>
+            UI/UX pensados e desenvolvidos baseado no alto conhecimento em interação humano computador (IHC).
+            </p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="feature4">
+          <div>
+            <h2>
+            Acompanhamento personalizado e
+            suporte ao longo do projeto
+            </h2>
+            <p>
+            Damos 100% de suporte e acompanhamento para os nossos clientes.
+            </p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="feature5">
+          <div>
+            <h2>
+            Suas informações estão seguras conosco!
+            </h2>
+            <p>
+            Fazemos nossos projetos com a maior segurança de criptografia para os nossos clientes
+            </p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="feature6">
+          <div>
+            <h2>
+            Dados exibidos de forma clara e organizada.
+            </h2>
+            <p>
+            Criando panoramas inteligíveis: apresentamos seus dados de forma clara e organizada para insights rápidos.
+            </p>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    (//pendulo com rgb com blur)
+    <div className="degrade">
+      <div>
+        <h1>Faça Seu Pedido Agora!</h1>
+        <h3>Acelere para o seu próximo nível de carreira com nossos sistemas</h3>
+        <button>Duvidas??? <img src="" alt="" /></button>
+        <div className="minidiv"><img src="" alt="" /> <h4>Tem alguma dúvida?</h4><h3>fale com nosso time!</h3></div>
+      </div>
+    </div>
+    <div className="FAQ">
+      <div className="faqtag">
+        <h1>Perguntas e Respostas mais frequentes</h1>
+      </div>
+      <div className="perguntas">
+        (// a fazer)
+      </div>
+      <div className="chamarwpp">
+        <img src="" alt="" />
+        <h1>Ficou com alguma dúvida?</h1>
+        <h3>Envie uma mensagem e converse com
+            uma pessoal real.</h3>
+        <button>CHAMAR NO WHATSAPP</button>
+      </div>
+    </div>
+    <footer>
+      <div className="insidefooter">
+        <div>
+        <section>
+          <div><img src="" alt="" /><h2>newsletter</h2><h1>Receba conteúdos inéditos e novidades
+            gratuitamente</h1></div>
+            </section>
+            <section><img src="" alt="" /><textarea name="" id="" placeholder='Digite seu e-mail'></textarea><button>ENVIAR</button></section>
+            <input type="text" /> <p>Autorizo o envio de comunicações por e-mail ou qualquer outro meio e concordo com os Termos e Políticas de privacidade.</p>
+          </div>
+          <div className="contatofooter">
+            <h2>Contato</h2>
+            <button>ENVIE UM E-MAIL</button>
+            <button><img src="" alt="" /><button>FALE CONOSCO</button></button>
+            <img src="" alt="" className="logo" />
+          </div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h3>B&B DevCode. Todos os direitos</h3><br />
+            <h3>reservados.</h3> <a href="">VOLTAR AO TOPO <img src="" alt="" /></a>
+          </div>
+      </div>
+    </footer>
+
+
+    </>
+  )
+}
+
+export default App
